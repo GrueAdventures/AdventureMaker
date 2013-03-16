40cd5fe0d9123b1fb599bd6ae4ad9545e3a5a1ae
Jose Labra Escudero <j.labra.escudero@gmail.com>
2013-03-16 13:47:53 -0400

    Coded up mock running program in Main to test viability of using Object Editor as UI. All objects were created correctly, though some work is needed in the display and generation of Vectors.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,28 @@
 package main;
 import objects.*;
 import bus.uigen.*;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
 	public static void main(String[] args){
-		ObjectEditor.edit(new Item("testItem", "Test Item", "This is an item", "", "", new Vector<String>()));
+		Scanner in = new Scanner(System.in);
+		String input;
+		while(true){
+			System.out.println("Please enter type of item to create:");
+			input = in.next();
+			if(input.equalsIgnoreCase("Item")){
+				ObjectEditor.edit(new Item("", "", "", "", "", new Vector<String>()));
+			}
+			else if(input.equalsIgnoreCase("Scene")){
+				ObjectEditor.edit(new Scene("", "", "", "", "", "", ""));
+			}
+			else if(input.equalsIgnoreCase("Event")){
+				ObjectEditor.edit(new Event("", new Vector<String>(), new Vector<Exec>(), new Vector<Report>()));
+			}
+			else{
+				System.out.println("Value entered is not a valid keyword.");
+			}
+		}
 	}
-
 }
