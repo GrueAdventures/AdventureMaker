b5c8db3beb1b867d4c1fe9806fd620a96939f8b7
Jose Labra Escudero <j.labra.escudero@gmail.com>
2013-04-01 12:35:06 -0400

    Changes to Event. Added add and remove methods for the vectors, added test suite in EventTest

--- src/objects/Event.java
+++ src/objects/Event.java
@@ -1,22 +1,23 @@
 package objects;
 
+import java.util.Iterator;
 import java.util.Vector;
 
-public class Event implements ObjectInterface{
+public class Event extends AbstractObject implements ObjectInterface{
 	//TODO: Populate this class
 	private String type;
 	private String id;
 	
-	private Vector<String> on;
-	private Vector<Exec> exec;
-	private Vector<Report> report;
+	private Vector<String> ons;
+	private Vector<Exec> execs;
+	private Vector<Report> reports;
 	
-	public Event(String id, Vector<String> on, Vector<Exec> exec, Vector<Report> report){
+	public Event(String id, Vector<String> ons, Vector<Exec> execs, Vector<Report> reports){
 		type = "event";
 		this.id = id;
-		this.on = on;
-		this.exec = exec;
-		this.report = report;
+		this.ons = ons;
+		this.execs = execs;
+		this.reports = reports;
 	}
 
 	public String getType() {
@@ -32,28 +33,94 @@ public class Event implements ObjectInterface{
 	}
 
 	public Vector<String> getOn() {
-		return on;
+		return ons;
 	}
 
-	public void setOn(Vector<String> on) {
-		this.on = on;
+	public void setOn(Vector<String> ons) {
+		this.ons = ons;
 	}
 
 	public Vector<Exec> getExec() {
-		return exec;
+		return execs;
 	}
 
-	public void setExec(Vector<Exec> exec) {
-		this.exec = exec;
+	public void setExec(Vector<Exec> execs) {
+		this.execs = execs;
 	}
 
 	public Vector<Report> getReport() {
-		return report;
+		return reports;
 	}
 
-	public void setReport(Vector<Report> report) {
-		this.report = report;
+	public void setReport(Vector<Report> reports) {
+		this.reports = reports;
 	}
 	
+	public void addReport(Report rep){
+		//Check to make sure event does not already contain the rep
+		if(!reports.contains(rep)){
+			reports.add(rep);
+		}
+		else
+			System.out.println("This report is already part of this event");
+	}
+	
+	//Removes report by numeric index
+	public void removeReport(int i){
+		reports.remove(i);
+	}
+	
+	public void addExec(Exec ex){
+		//Check to make sure event does not already contain the ex
+		if(!execs.contains(ex)){
+			execs.add(ex);
+		}
+		else
+			System.out.println("This Exec is already part of this event");
+	}
+	
+	//Removes report by numeric index
+	public void removeExec(int i){
+		execs.remove(i);
+	}
+	
+	public void addOn(String condition){
+		//Check to make sure event does not already contain the condition
+		if(!ons.contains(condition)){
+			ons.add(condition);
+		}
+		else
+			System.out.println("This condition is already part of this event");
+	}
+	
+	//Removes condition by numeric index
+	public void removeOn(int i){
+		ons.remove(i);
+	}
+	
+//	public Item getItemById(String id){
+//		Iterator<Item> itemItr = items.iterator();
+//		Item temp;
+//		while(itemItr.hasNext()){
+//			temp = itemItr.next();
+//			if(temp.equals(id)) return temp;
+//		}
+//		System.out.println("returning null");
+//		return null;
+//	}
+//	
+//	public boolean hasItem(String id) {
+//		//The outside class will have to determine whether or not two objects are equals
+//		Iterator<Item> itemItr = items.iterator();
+//		while(itemItr.hasNext()){
+//			if(itemItr.next().getId().equals(id)) return true;
+//		}
+//		return false;
+//	}
+//	
+//	public boolean hasItem(Item item){
+//		return items.contains(item);
+//	}
+
 
 }
--- src/tests/EventTest.java
+++ src/tests/EventTest.java
@@ -1,6 +1,8 @@
 package tests;
 
 import static org.junit.Assert.*;
+import objects.*;
+import java.util.Vector;
 
 import org.junit.Test;
 
@@ -8,52 +10,109 @@ public class EventTest {
 
 	@Test
 	public void testEvent() {
-		fail("Not yet implemented");
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		assertTrue(event.getId().equals("testEvent")
+				&&event.getType().equals("event")
+				&&event.getOn().isEmpty()
+				&&event.getExec().isEmpty()
+				&&event.getReport().isEmpty());
 	}
-
+	
 	@Test
-	public void testGetType() {
-		fail("Not yet implemented");
+	public void testAddReport(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addReport(new Report("Description of report", "Narration of report"));
+		assertTrue(!event.getReport().isEmpty());
+		assertTrue(event.getReport().get(0).getDescription().equals("Description of report"));
+		
+		event.addReport(new Report("Description of report2", "Narration of report2"));
+		assertTrue(!event.getReport().isEmpty());
+		assertTrue(event.getReport().get(0).getDescription().equals("Description of report"));
+		assertTrue(event.getReport().get(1).getDescription().equals("Description of report2"));
 	}
-
+	
 	@Test
-	public void testGetId() {
-		fail("Not yet implemented");
+	public void testRemoveReport(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addReport(new Report("Description of report", "Narration of report"));
+		event.addReport(new Report("Description of report2", "Narration of report2"));
+		event.addReport(new Report("Description of report3", "Narration of report3"));
+		
+		assertTrue(!event.getReport().isEmpty());
+		assertTrue(event.getReport().size() == 3);
+		assertTrue(event.getReport().get(1).getDescription().equals("Description of report2"));
+		
+		event.removeReport(1);
+		assertTrue(!event.getReport().isEmpty());
+		assertTrue(event.getReport().size() == 2);
+		assertTrue(event.getReport().get(1).getDescription().equals("Description of report3"));
 	}
-
+	
 	@Test
-	public void testSetId() {
-		fail("Not yet implemented");
+	public void testAddExec(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addExec(new Exec("Action to execute", new Vector<String>()));
+		assertTrue(!event.getExec().isEmpty());
+		assertTrue(event.getExec().get(0).getAction().equals("Action to execute"));
+		
+		event.addExec(new Exec("Action to execute2", new Vector<String>()));
+		assertTrue(!event.getExec().isEmpty());
+		assertTrue(event.getExec().get(0).getAction().equals("Action to execute"));
+		assertTrue(event.getExec().get(1).getAction().equals("Action to execute2"));
 	}
-
+	
 	@Test
-	public void testGetOn() {
-		fail("Not yet implemented");
+	public void testRemoveExec(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addExec(new Exec("Action to execute", new Vector<String>()));
+		event.addExec(new Exec("Action to execute2", new Vector<String>()));
+		event.addExec(new Exec("Action to execute3", new Vector<String>()));
+		
+		assertTrue(!event.getExec().isEmpty());
+		assertTrue(event.getExec().size() == 3);
+		assertTrue(event.getExec().get(0).getAction().equals("Action to execute"));
+		
+		event.removeExec(1);
+		assertTrue(!event.getExec().isEmpty());
+		assertTrue(event.getExec().size() == 2);
+		assertTrue(event.getExec().get(1).getAction().equals("Action to execute3"));
+		
 	}
-
+	
 	@Test
-	public void testSetOn() {
-		fail("Not yet implemented");
+	public void testAddOn(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addOn("Condition");
+		assertTrue(!event.getOn().isEmpty());
+		assertTrue(event.getOn().get(0).equals("Condition"));
+		
+		event.addOn("Condition2");
+		assertTrue(!event.getOn().isEmpty());
+		assertTrue(event.getOn().get(0).equals("Condition"));
+		assertTrue(event.getOn().get(1).equals("Condition2"));
 	}
-
+	
 	@Test
-	public void testGetExec() {
-		fail("Not yet implemented");
+	public void testRemoveOn(){
+		Event event = new Event("testEvent", new Vector<String>(), new Vector<Exec>(), new Vector<Report>());
+		
+		event.addOn("Condition");
+		event.addOn("Condition2");
+		event.addOn("Condition3");
+		
+		assertTrue(!event.getOn().isEmpty());
+		assertTrue(event.getOn().size() == 3);
+		assertTrue(event.getOn().get(1).equals("Condition2"));
+		
+		event.removeExec(1);
+		assertTrue(!event.getOn().isEmpty());
+		assertTrue(event.getOn().size() == 2);
+		assertTrue(event.getOn().get(1).equals("Condition3"));
 	}
-
-	@Test
-	public void testSetExec() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetReport() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSetReport() {
-		fail("Not yet implemented");
-	}
-
+	
 }
